4209752249d7e929fbe60404b76be8c4ef8dcff8
Livio Amstutz <livio.a@gmail.com>
2017-08-22 17:11:42 +0200

    list members and start detail

--- js/app.js
+++ js/app.js
@@ -6,12 +6,43 @@ $(document).ready(function () {
 });
 
 function receiveMembers() {
+    call('users', function(data){
+        console.log(data);
+        data.forEach(function(user) {
+            var content = '<li onclick="memberDetail('+ user.UserId +')">' + user.FirstName + ' ' + user.LastName + '</li>';
+            $('ul#memberlist').append(content);
+        }, this);
+    });
+}
+
+function memberDetail(id) {
+    storeObject.id = id;
+    //Change page
+    $.mobile.changePage("#members-detail");
+}
+
+var storeObject = {
+    id: 0
+}
+
+$(document).on('pagebeforeshow', '#members-detail', function(){     
+    $('#test').text(storeObject.id)
+});
+
+
+var basePath = 'https://zbw.lump.ch/api/v1';
+
+function call(path, successFn) {
+    if(path.indexOf('/') != 0) {
+        path = '/'+path
+    }
     $.ajax({
-        url: "https://zbw.lump.ch/api/v1/users",
-        username: 'hans.keller@optimatik.ch',
-        password: 'HelloWorld17',
-        success: function(){
-            console.log("test");
-        }
+        type: 'GET',
+        url: basePath+path,
+        dataType: 'json',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ('Authorization', 'Basic ' + btoa('livio.a@gmail.com' + ":" + 'test1234'));
+        },
+        success: successFn
     });
 }
