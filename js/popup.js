744f659b357e6de78981a7215756a5419a01277b
Livio Amstutz <livio.a@gmail.com>
2017-09-21 08:52:13 +0200

    popup

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,16 @@
+/**
+ * @class popup
+ */
+
+/**
+ * Method for showing messages inside a popup div
+ * @method popup
+ * @param {*id of the popup div} id 
+ * @param {* title to be shown as header} title 
+ * @param {* text to be shown} text 
+ * @param {* timeout before auto closing} timeout 
+ */
+
 function popup(id, title, text, timeout) {
     $('#' + id + '-title').text(title)
     $('#' + id + '-text').text(text)
